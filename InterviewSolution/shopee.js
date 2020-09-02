c62c67197d5fba37e3e65a49ec70a58bceedc88a
丁超凡 <dingchaofan@dingchaofandeMacBook-Pro.local>
2020-09-02 11:34:35 +0800

    shopee

---
+++ InterviewSolution/shopee.js
@@ -0,0 +1,29 @@
+// 1.
+// 判断一串数字是不是靓号
+// 靓号条件：1.至少存在连续4个相同数字；2.至少存在4个连续递增为1的数字
+
+var numStr = readline();
+
+function isNiceNum(numStr){
+    // code
+    return Y；
+    return N
+}
+console.log(isNiceNum(numStr))
+
+
+// 3. 求笛卡尔积
+// x = ['a1','a2'] y=['b1',b2]
+// x*b = [['a1','b1'],['a1','b2'],['a2','b1'],['a2','b2']]
+/**
+ * 
+ * @param xSet string字符串一维数组 
+ * @param ySet string字符串一维数组 
+ * @return string字符串二维数组
+ */
+function getCartesian( xSet ,  ySet ) {
+    // write code here
+}
+module.exports = {
+    getCartesian : getCartesian
+};
